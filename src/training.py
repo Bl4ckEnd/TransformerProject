7a0a53f94a08d53ba73c6ee875f6417efa01feb3
Valentin Gölz <vale.goelz@gmail.com>
2023-02-27 11:06:18 +0100

    put training and testing inside functions

--- src/training.py
+++ src/training.py
@@ -8,65 +8,72 @@ from tqdm import tqdm
 from datetime import datetime as dt
 from utilities import set_device
 from utilities import load_params
-
-# Load params
-(
-    data_path,
-    batch_size,
-    seq_length,
-    N,
-    d_model,
-    d_ff,
-    h,
-    amount_of_data,
-    learning_rate,
-    epochs,
-    SAVE_PATH,
-    device_name,
-) = load_params()
-
-# set device
-device = set_device(device_name)
-
-# Load data
-data = pd.read_csv(data_path)
-data = data.sample(frac=amount_of_data, random_state=42)
-train_loader, test_loader, vocab_len = data_processing(
-    data, batch_size=batch_size, seq_length=seq_length, test_size=0.2
-)
-
-# Create model
-model = make_model(
-    vocab_len, N=N, d_model=d_model, d_ff=d_ff, h=h, seq_length=seq_length
-)
-
-# move model to device
-model = model.to(device)
-
-# Set loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+import streamlit as st
 
 
-# set to training mode
-model.train()
+def train():
+    # Load params
+    (
+        data_path,
+        batch_size,
+        seq_length,
+        N,
+        d_model,
+        d_ff,
+        h,
+        amount_of_data,
+        learning_rate,
+        epochs,
+        SAVE_PATH,
+        device_name,
+    ) = load_params()
 
-# Train model
-for e in tqdm(range(epochs)):
-    running_loss = 0
-    for x, y in tqdm(train_loader):
-        # move data to device
-        x = x.to(device)
-        y = y.to(device)
-        optimizer.zero_grad()
-        y_pred = model(x)
-        loss = criterion(y_pred, y)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-    else:
-        print(f"Training loss: {running_loss / len(train_loader)}")
+    # set device
+    device = set_device(device_name)
 
-# Save model
-time = dt.now().strftime("%Y-%m-%d_%H-%M-%S")
-torch.save(model, SAVE_PATH + time + ".pth")
+    # Load data
+    data = pd.read_csv(data_path)
+    data = data.sample(frac=amount_of_data, random_state=42)
+    train_loader, test_loader, vocab_len = data_processing(
+        data, batch_size=batch_size, seq_length=seq_length, test_size=0.2
+    )
+
+    # Create model
+    model = make_model(
+        vocab_len, N=N, d_model=d_model, d_ff=d_ff, h=h, seq_length=seq_length
+    )
+
+    # move model to device
+    model = model.to(device)
+
+    # Set loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # set to training mode
+    model.train()
+
+    # Train model
+    for e in tqdm(range(epochs)):
+        st.progress(e / epochs)
+        running_loss = 0
+        for x, y in tqdm(train_loader):
+            # move data to device
+            x = x.to(device)
+            y = y.to(device)
+            optimizer.zero_grad()
+            y_pred = model(x)
+            loss = criterion(y_pred, y)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        else:
+            print(f"Training loss: {running_loss / len(train_loader)}")
+
+    # Save model
+    time = dt.now().strftime("%Y-%m-%d_%H-%M-%S")
+    torch.save(model, SAVE_PATH + time + ".pth")
+
+
+if __name__ == "__main__":
+    train()
