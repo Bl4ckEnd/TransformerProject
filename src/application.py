5d133f1d094a144ed745746a3a1b713e59209d8e
Valentin Gölz <vale.goelz@gmail.com>
2023-03-06 10:48:01 +0100

    added new features to application.py

--- src/application.py
+++ src/application.py
@@ -2,11 +2,12 @@ import streamlit as st
 import yaml
 
 # let user decide in streamlit application which parameters to use for training
-st.text("Choose hyperparameters")
-h = st.slider("h", 1, 8, 2)
-N = st.slider("N", 1, 6, 2)
-epochs = st.slider("epochs", 1, 10, 5)
-n_samples = st.slider("n_samples", 5000, 50000, 10000, step=5000)
+st.text("Methods in Artificial Intelligence - Transformer Project")
+st.text("Step 1. Choose hyperparameters")
+h = st.slider("h (Number of Attention-Heads)", 1, 8, 2)
+N = st.slider("N (Number of Attention-Layers)", 1, 6, 2)
+epochs = st.slider("Epochs", 1, 10, 5)
+n_samples = st.slider("Number of Samples", 5000, 50000, 10000, step=5000)
 
 # confirmed variables
 if "confirmed" not in st.session_state:
@@ -14,6 +15,9 @@ if "confirmed" not in st.session_state:
 
 if "training_finished" not in st.session_state:
     st.session_state.training_finished = False
+
+if "testing_finished" not in st.session_state:
+    st.session_state.testing_finished = False
 
 if st.button("Confirm parameters"):
     # change parameters in params.yaml
@@ -35,6 +39,7 @@ if st.button("Confirm parameters"):
 
 if st.session_state.confirmed:
     st.write("Parameters confirmed")
+    st.text("Step 2. Start training.")
 
 if st.button("Start training") and st.session_state.confirmed:
     # start training
@@ -45,9 +50,14 @@ if st.button("Start training") and st.session_state.confirmed:
     st.write("Training finished")
     st.session_state.training_finished = True
 
-if st.button("Start test") and st.session_state.training_finished:
-    from testing import test
+if st.session_state.training_finished:
+    if st.button("Start test"):
+        from testing import test
 
-    st.write("Test started")
-    test(st.session_state.model)
-    st.write("Test completed")
+        st.write("Test started")
+        test(st.session_state.model)
+        st.write("Test completed")
+        st.session_state.testing_finished = True
+
+if st.session_state.testing_finished:
+    st.write("Session finished.")
